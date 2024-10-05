d11c9320ba38ce2ffd651def6137926c92fcfca3
eyobScript <eyobbmulugeta@gmaol.com>
2024-10-05 19:30:08 +0300

    Row Of Movies

--- netflix-clone/src/Components/Rows/RowList/RowList.js
+++ netflix-clone/src/Components/Rows/RowList/RowList.js
@@ -1,52 +1,34 @@
-import React from 'react'
-import Row from '../Row/Row'
-import requests from '../../../Utils/requests'
+import React from 'react';
+import Row from '../Row/Row';
+import requests from '../../../Utils/requests';
 
 function RowList() {
-  return (
-    <>
-    <Row 
-      title="NETFLIX ORIGINAL"
-      fetchUrl={requests.fetchNetflixOriginals} 
-      isLargeRow={true}
-      />
-      <Row 
-      title="Trending Now"
-      fetchUrl={requests.fetchTrending}
-      
-      />
-      <Row 
-      title="Top Rated"
-      fetchUrl={requests.fetchTopRatedMovies} 
-      />
-      <Row 
-      title="Action Movies"
-      fetchUrl={requests.fetchActionMovies} 
-      />
-      <Row 
-      title="Comedy Movies"
-      fetchUrl={requests.fetchComedyMovies} 
-      />
-      <Row 
-      title="Horror Movies"
-      fetchUrl={requests.fetchHorrorMovies} 
-      />
-      <Row 
-      title="Romance Movies"
-      fetchUrl={requests.fetchRomanceMovies} 
-      
-      />
-      <Row 
-      title="Documentaries"
-      fetchUrl={requests.fetchDocumentaries} 
-      />
-      <Row 
-      title="Tv Shows"
-      fetchUrl={requests.fetchTvShow} 
-      />
-      
-    </>
-  )
+    // Create an array of objects with title and fetchUrl
+    const rowConfigs = [
+        { title: "NETFLIX ORIGINAL", fetchUrl: requests.fetchNetflixOriginals, isLargeRow: true },
+        { title: "Trending Now", fetchUrl: requests.fetchTrending },
+        { title: "Top Rated", fetchUrl: requests.fetchTopRatedMovies },
+        { title: "Action Movies", fetchUrl: requests.fetchActionMovies },
+        { title: "Comedy Movies", fetchUrl: requests.fetchComedyMovies },
+        { title: "Horror Movies", fetchUrl: requests.fetchHorrorMovies },
+        { title: "Romance Movies", fetchUrl: requests.fetchRomanceMovies },
+        { title: "Documentaries", fetchUrl: requests.fetchDocumentaries },
+        { title: "TV Shows", fetchUrl: requests.fetchTvShow },
+    ];
+
+    return (
+        <>
+            {/* Map through the rowConfigs array to render Row components */}
+            {rowConfigs.map((row, index) => (
+                <Row
+                    key={index} // Use index as the key; it's acceptable here since the array is static
+                    title={row.title}
+                    fetchUrl={row.fetchUrl}
+                    isLargeRow={row.isLargeRow} // Pass isLargeRow if it's defined
+                />
+            ))}
+        </>
+    );
 }
 
-export default RowList
+export default RowList;
